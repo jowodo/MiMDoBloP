51e174cc50042be8e3441c828d2cb18ef56bb3f4
pur <j.w.dorn@gmx.at>
2022-03-04 18:33:00 +0100

    added creation/lastmod date

--- res/res.php
+++ res/res.php
@@ -2,6 +2,7 @@
 $HOMEDIR="https://wlankabel.at/john/blog";
 function startit($title)
 {
+	GLOBAL $HOMEDIR;
 	echo "<!DOCTYPE html>";
 	echo "<html>";
 	echo "<head>";
@@ -24,7 +25,18 @@ function startit($title)
 
 function doit($dir)
 {
-	
+	$AUTHOR=shell_exec("ls -l ./index.md | awk '{print $3}'"); 
+	$DIRNAME=shell_exec("basename `pwd`"); 
+	$DATE=substr($DIRNAME,0,10);
+	$MONTH_LASTCHANGE=shell_exec("ls -l ./index.md | awk '{print $6,$7}'");
+	$LASTCHANGE=shell_exec("ls -l ./index.md | awk '{print $8}'"); 
+	$YEAR=$LASTCHANGE;
+	# if the file was edited this year this column is the time instead of year
+	# and therefore the length of the var is 5(+1)
+	if (strlen($LASTCHANGE) == 6){
+		$YEAR=date('Y');
+	}
+	echo "created by $AUTHOR on $DATE - last changed on $YEAR $MONTH_LASTCHANGE";
 	echo "<article>";
 	$body = shell_exec("/usr/bin/tail -n+3 $dir/index.md | /usr/bin/markdown ") ; 
 	echo $body;
