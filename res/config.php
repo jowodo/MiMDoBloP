f860ef79bfed91c6a6fbba02293879ee364b3b62
pur <j.w.dorn@gmx.at>
2022-05-07 23:29:17 +0200

    added .note to ignore in res/config.php

--- res/config.php
+++ res/config.php
@@ -3,5 +3,5 @@ $MD_EXECUTABLE="/usr/bin/markdown";
 $HOMEURL="https://blog.wlankabel.at/";
 $HOMEPATH="/var/www/html/blog";
 $PAGENAME="Mimdoblop";
-$EXCLUDES=['index.php', 'res', '.git', '.htaccess', '.', '..'];
+$EXCLUDES=['index.php', 'res', '.git', '.htaccess', '.', '..', '.note'];
 ?>
