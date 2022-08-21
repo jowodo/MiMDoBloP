81328a61a1ac5cfa505de0a26f1b0cca246e732d
pur <j.w.dorn@gmx.at>
2022-08-21 11:08:18 +0200

    get_read_time

--- res/config.php
+++ res/config.php
@@ -1,8 +1,9 @@
 <?php
-$MD_EXECUTABLE="/usr/bin/markdown";
+$MD_EXECUTABLE="/usr/local/bin/markdown";
 $HOMEURL="https://blog.wlankabel.at/";
 $HOMEPATH="/var/www/html/blog";
 $PAGENAME="Mimdoblop";
 $EXCLUDES=['index.php', 'res', '.git', '.htaccess', '.', '..', '.note'];
 $REVERSE_CHRON=true;
+$WORDSPERMINUTE=100;
 ?>
